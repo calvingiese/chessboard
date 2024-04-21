ed1b65fdab034e611ba03a472ebbc27c2855e0bc
calvingiese <giesecalvin@gmail.com>
2024-04-21 15:11:15 -0600

    feat: add sidebar to app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,48 @@
 <script setup lang="ts">
+  import { theme } from './plugins/vuetify'
+  import { onMounted, type Ref, ref } from 'vue'
   import ChessBoard from './components/ChessBoard.vue'
+  import SelectionSummary from './components/SelectionSummary.vue'
+
+  onMounted(() => {
+    resize()
+  })
+
+  window.addEventListener("resize", resize)
+  function resize() {
+    resizeChessboardContainer()
+    resizeChessboard()
+    orientNavigation()
+  }
+
+  function resizeChessboardContainer() {
+    let nav = document.getElementById('nav').className
+    let navWidth = (document.getElementById('nav').offsetWidth) + 'px'
+    const navIsOnRight = nav.includes('v-navigation-drawer--right')
+    
+    let container = document.getElementById('chessboard-container')
+    container.style.width = navIsOnRight ? 'calc(100vw - 17px - ' + navWidth + ')' : '100vw'
+    container.style.height = navIsOnRight ? '100vh' : 'calc(100vh - 400px)'
+  }
+
+  function resizeChessboard() {
+    let container = document.getElementById('chessboard-container')
+
+    const width = container.offsetWidth
+    const height = container.offsetHeight
+    const minLength = (width > height ? height : width) + 'px'
+
+    let chessboard = document.getElementById('chessboard')
+    chessboard.style.width = minLength
+    chessboard.style.height = minLength
+  }
+
+  let isMobile = ref(false)
+  function orientNavigation() {
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+      isMobile.value = true
+    }
+  }
 
   let selections: Ref<string[]> = ref([])
   function addSelection(square: string) {
@@ -14,6 +57,9 @@
 
 <template>
   <v-layout>
+    <v-navigation-drawer id="nav" permanent :location="isMobile ? 'bottom' : 'right'" style="min-height: 400px;"> 
+      <SelectionSummary id="selection-summary" :selections="selections" />
+    </v-navigation-drawer>
     <div id="chessboard-container" class="d-flex align-center justify-center">
       <ChessBoard id="chessboard" @select="addSelection" />
     </div>
