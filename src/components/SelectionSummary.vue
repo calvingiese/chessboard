5790511b084fc0376393feb6cb7c3e0ee02261c2
calvingiese <giesecalvin@gmail.com>
2024-04-21 15:10:39 -0600

    feat: display selections on sidebar

--- src/components/SelectionSummary.vue
+++ src/components/SelectionSummary.vue
@@ -3,9 +3,30 @@
 </script>
 
 <template>
-
+  <v-table class="pa-1 rounded" density="compact">
+    <thead class="bg-plain">
+      <tr>
+        <th>Order</th>
+        <th>Square</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="selection in selections">
+        <td>{{ (selections.indexOf(selection) + 1) + "." }}</td>
+        <td>{{ selection }}</td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
 
 <style>
+  tbody tr:nth-of-type(odd) {
+	  background-color: rgb(var(--v-theme-light));
+    color: black;
+  }
 
+  tbody tr:nth-of-type(even) {
+	  background-color: rgb(var(--v-theme-dark));
+    color: white;
+  }
 </style>
