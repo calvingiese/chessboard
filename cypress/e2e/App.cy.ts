2db68a528819f084e5b36a818792f3674470fe34
calvingiese <giesecalvin@gmail.com>
2024-04-21 15:10:09 -0600

    test: chessboard interactions

--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -23,4 +23,19 @@ describe('Chess App', () => {
     cy.get('#nav').should('have.class', 'v-navigation-drawer--right')
   })
 
+  describe('interacting with chessboard', () => {
+    it('adds selections when clicking square', () => {
+      cy.get('#chessboard').get('#a1').click()
+      cy.get('#chessboard').get('#a2').click()
+      
+      cy.get('#selection-summary').get('tr').should('have.length', 3)
+    })
+
+    it('doesnt add selection for duplicate', () => {
+      cy.get('#chessboard').get('#a1').click()
+      cy.get('#chessboard').get('#a1').click()
+      
+      cy.get('#selection-summary').get('tr').should('have.length', 2)
+    })
+  })
 })
